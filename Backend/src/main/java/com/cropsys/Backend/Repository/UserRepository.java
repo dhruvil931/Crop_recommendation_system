aa28cd2085a6c152ee80488b980f1f6a8dc05f84
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-04-27 13:16:25 +0530

    Update UserRepository

--- Backend/src/main/java/com/cropsys/Backend/Repository/UserRepository.java
+++ Backend/src/main/java/com/cropsys/Backend/Repository/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cropsys.Backend.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long>{
 
   
