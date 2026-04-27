79e710f07c27fe487c3fc436ef7c98273235decf
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-04-27 15:19:12 +0530

    Fix data.sql duplicate constraint issue

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -10,12 +10,13 @@ ON CONFLICT (role_name) DO NOTHING;
 -- Admin user (password = admin)
 INSERT INTO users (username, email, password, location, phone_number)
 VALUES (
-    'Admin',
-    'admin@example.com',
-    '$2a$10$7EqJtq98hPqEX7fNZaFWoOHi7nXz6xXxV9I9B1wFqk7RZ4p8zG9lS',
-    'GUJARAT',
-    '9999999999'
-);
+           'Admin',
+           'admin@example.com',
+           '$2a$10$7EqJtq98hPqEX7fNZaFWoOHi7nXz6xXxV9I9B1wFqk7RZ4p8zG9lS',
+           'GUJARAT',
+           '9999999999'
+       )
+    ON CONFLICT (email) DO NOTHING;
 
 -- User
 INSERT INTO users (username, email, password, location, phone_number)
@@ -25,17 +26,21 @@ VALUES (
     '$2a$10$7EqJtq98hPqEX7fNZaFWoOHi7nXz6xXxV9I9B1wFqk7RZ4p8zG9lS',
     'GUJARAT',
     '8888888888'
-);
+)
+    ON CONFLICT (email) DO NOTHING;;
 
 -- Role mapping
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.role_id FROM users u, roles r
-WHERE u.email = 'admin@example.com' AND r.role_name = 'ROLE_ADMIN';
+WHERE u.email = 'admin@example.com' AND r.role_name = 'ROLE_ADMIN'
+ON CONFLICT DO NOTHING;
 
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.role_id FROM users u, roles r
-WHERE u.email = 'admin@example.com' AND r.role_name = 'ROLE_USER';
+WHERE u.email = 'admin@example.com' AND r.role_name = 'ROLE_USER'
+ON CONFLICT DO NOTHING;
 
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.role_id FROM users u, roles r
-WHERE u.email = 'user@example.com' AND r.role_name = 'ROLE_USER';
+WHERE u.email = 'user@example.com' AND r.role_name = 'ROLE_USER'
+ON CONFLICT DO NOTHING;
